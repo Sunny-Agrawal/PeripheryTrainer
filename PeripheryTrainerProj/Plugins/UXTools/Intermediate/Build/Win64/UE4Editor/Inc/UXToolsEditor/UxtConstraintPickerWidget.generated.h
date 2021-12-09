778083b41edb092a64c48df4d6cc2bc36727d390
Sunny Agrawal <sunny.agrawal@utexas.edu>
2021-12-09 13:37:12 -0600

    Getting started

---
+++ PeripheryTrainerProj/Plugins/UXTools/Intermediate/Build/Win64/UE4Editor/Inc/UXToolsEditor/UxtConstraintPickerWidget.generated.h
@@ -0,0 +1,122 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+class UUxtTransformConstraint;
+#ifdef UXTOOLSEDITOR_UxtConstraintPickerWidget_generated_h
+#error "UxtConstraintPickerWidget.generated.h already included, missing '#pragma once' in UxtConstraintPickerWidget.h"
+#endif
+#define UXTOOLSEDITOR_UxtConstraintPickerWidget_generated_h
+
+#define HostProject_Plugins_UXTools_Source_UXToolsEditor_Public_UxtConstraintPickerWidget_h_20_SPARSE_DATA
+#define HostProject_Plugins_UXTools_Source_UXToolsEditor_Public_UxtConstraintPickerWidget_h_20_RPC_WRAPPERS \
+ \
+	DECLARE_FUNCTION(execAddConstraint); \
+	DECLARE_FUNCTION(execGetConstraintDescription); \
+	DECLARE_FUNCTION(execGetConstraintName); \
+	DECLARE_FUNCTION(execGetConstraintClasses); \
+	DECLARE_FUNCTION(execHasValidOwner);
+
+
+#define HostProject_Plugins_UXTools_Source_UXToolsEditor_Public_UxtConstraintPickerWidget_h_20_RPC_WRAPPERS_NO_PURE_DECLS \
+ \
+	DECLARE_FUNCTION(execAddConstraint); \
+	DECLARE_FUNCTION(execGetConstraintDescription); \
+	DECLARE_FUNCTION(execGetConstraintName); \
+	DECLARE_FUNCTION(execGetConstraintClasses); \
+	DECLARE_FUNCTION(execHasValidOwner);
+
+
+#define HostProject_Plugins_UXTools_Source_UXToolsEditor_Public_UxtConstraintPickerWidget_h_20_EVENT_PARMS
+#define HostProject_Plugins_UXTools_Source_UXToolsEditor_Public_UxtConstraintPickerWidget_h_20_CALLBACK_WRAPPERS
+#define HostProject_Plugins_UXTools_Source_UXToolsEditor_Public_UxtConstraintPickerWidget_h_20_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUUxtConstraintPickerWidget(); \
+	friend struct Z_Construct_UClass_UUxtConstraintPickerWidget_Statics; \
+public: \
+	DECLARE_CLASS(UUxtConstraintPickerWidget, UEditorUtilityWidget, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/UXToolsEditor"), NO_API) \
+	DECLARE_SERIALIZER(UUxtConstraintPickerWidget)
+
+
+#define HostProject_Plugins_UXTools_Source_UXToolsEditor_Public_UxtConstraintPickerWidget_h_20_INCLASS \
+private: \
+	static void StaticRegisterNativesUUxtConstraintPickerWidget(); \
+	friend struct Z_Construct_UClass_UUxtConstraintPickerWidget_Statics; \
+public: \
+	DECLARE_CLASS(UUxtConstraintPickerWidget, UEditorUtilityWidget, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/UXToolsEditor"), NO_API) \
+	DECLARE_SERIALIZER(UUxtConstraintPickerWidget)
+
+
+#define HostProject_Plugins_UXTools_Source_UXToolsEditor_Public_UxtConstraintPickerWidget_h_20_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UUxtConstraintPickerWidget(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UUxtConstraintPickerWidget) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UUxtConstraintPickerWidget); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UUxtConstraintPickerWidget); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UUxtConstraintPickerWidget(UUxtConstraintPickerWidget&&); \
+	NO_API UUxtConstraintPickerWidget(const UUxtConstraintPickerWidget&); \
+public:
+
+
+#define HostProject_Plugins_UXTools_Source_UXToolsEditor_Public_UxtConstraintPickerWidget_h_20_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UUxtConstraintPickerWidget(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UUxtConstraintPickerWidget(UUxtConstraintPickerWidget&&); \
+	NO_API UUxtConstraintPickerWidget(const UUxtConstraintPickerWidget&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UUxtConstraintPickerWidget); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UUxtConstraintPickerWidget); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UUxtConstraintPickerWidget)
+
+
+#define HostProject_Plugins_UXTools_Source_UXToolsEditor_Public_UxtConstraintPickerWidget_h_20_PRIVATE_PROPERTY_OFFSET \
+	FORCEINLINE static uint32 __PPO__TabID() { return STRUCT_OFFSET(UUxtConstraintPickerWidget, TabID); }
+
+
+#define HostProject_Plugins_UXTools_Source_UXToolsEditor_Public_UxtConstraintPickerWidget_h_17_PROLOG \
+	HostProject_Plugins_UXTools_Source_UXToolsEditor_Public_UxtConstraintPickerWidget_h_20_EVENT_PARMS
+
+
+#define HostProject_Plugins_UXTools_Source_UXToolsEditor_Public_UxtConstraintPickerWidget_h_20_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	HostProject_Plugins_UXTools_Source_UXToolsEditor_Public_UxtConstraintPickerWidget_h_20_PRIVATE_PROPERTY_OFFSET \
+	HostProject_Plugins_UXTools_Source_UXToolsEditor_Public_UxtConstraintPickerWidget_h_20_SPARSE_DATA \
+	HostProject_Plugins_UXTools_Source_UXToolsEditor_Public_UxtConstraintPickerWidget_h_20_RPC_WRAPPERS \
+	HostProject_Plugins_UXTools_Source_UXToolsEditor_Public_UxtConstraintPickerWidget_h_20_CALLBACK_WRAPPERS \
+	HostProject_Plugins_UXTools_Source_UXToolsEditor_Public_UxtConstraintPickerWidget_h_20_INCLASS \
+	HostProject_Plugins_UXTools_Source_UXToolsEditor_Public_UxtConstraintPickerWidget_h_20_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define HostProject_Plugins_UXTools_Source_UXToolsEditor_Public_UxtConstraintPickerWidget_h_20_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	HostProject_Plugins_UXTools_Source_UXToolsEditor_Public_UxtConstraintPickerWidget_h_20_PRIVATE_PROPERTY_OFFSET \
+	HostProject_Plugins_UXTools_Source_UXToolsEditor_Public_UxtConstraintPickerWidget_h_20_SPARSE_DATA \
+	HostProject_Plugins_UXTools_Source_UXToolsEditor_Public_UxtConstraintPickerWidget_h_20_RPC_WRAPPERS_NO_PURE_DECLS \
+	HostProject_Plugins_UXTools_Source_UXToolsEditor_Public_UxtConstraintPickerWidget_h_20_CALLBACK_WRAPPERS \
+	HostProject_Plugins_UXTools_Source_UXToolsEditor_Public_UxtConstraintPickerWidget_h_20_INCLASS_NO_PURE_DECLS \
+	HostProject_Plugins_UXTools_Source_UXToolsEditor_Public_UxtConstraintPickerWidget_h_20_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> UXTOOLSEDITOR_API UClass* StaticClass<class UUxtConstraintPickerWidget>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID HostProject_Plugins_UXTools_Source_UXToolsEditor_Public_UxtConstraintPickerWidget_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
